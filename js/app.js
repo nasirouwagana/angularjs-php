203d274d029d148238ac0c498f6a62bcc0f26d22
Nasirou <nasirou@tbc.com>
2014-10-11 09:19:52 +0100

    First Commit

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@
 
 var myApp = angular.module('myApp', ['ngRoute']);
 
-var Etudiants = myApp.factory('Etudiants', function($http, $q, $timeout) {
+var Etudiants = myApp.factory('Etudiants', function ($http, $q, $timeout) {
     var factory = {
         etudiants: false,
-        getEtudiants: function() {
+        getEtudiants: function () {
             var deferred = $q.defer();
-            $http.get('data.php').success(function(data) {
+            $http.get('data.php').success(function (data) {
                 factory.etudiants = data;
-                $timeout(function() {
-                    deferred.resolve(factory.etudiants);                    
+                $timeout(function () {
+                    deferred.resolve(factory.etudiants);
                 }, 3000);
-            }).error(function(data) {
+            }).error(function (data) {
                 deferred.reject('Impossible de recuperer la liste des etudiants');
             });
 
@@ -23,21 +23,21 @@ var Etudiants = myApp.factory('Etudiants', function($http, $q, $timeout) {
     return factory;
 });
 
-var EtudiantsCtrl = myApp.controller('EtudiantsCtrl', function($scope, Etudiants) {
+var EtudiantsCtrl = myApp.controller('EtudiantsCtrl', function ($scope, Etudiants) {
     $scope.loading = true;
-    $scope.etudiants = Etudiants.getEtudiants().then(function(etudiants) {
+    $scope.etudiants = Etudiants.getEtudiants().then(function (etudiants) {
         $scope.loading = false;
         $scope.etudiants = etudiants;
-    }, function(msg) {
+    }, function (msg) {
         alert(msg);
     });
 });
 
-var EtudiantCtrl = myApp.controller('EtudiantCtrl', function($scope, $routeParams) {
+var EtudiantCtrl = myApp.controller('EtudiantCtrl', function ($scope, $routeParams) {
     $scope.params = $routeParams;
 });
 
-myApp.config(function($routeProvider) {
+myApp.config(function ($routeProvider) {
     $routeProvider
             .when('/', {templateUrl: 'partials/home.php', controller: 'EtudiantsCtrl'})
             .when('/etudiant/:idetudiant', {templateUrl: 'partials/etudiant.php', controller: 'EtudiantCtrl'})
