fef39e6c5de51d91580fb5f69c515041173bcd82
Nasirou <nasirou@tbc.com>
2014-10-11 14:43:15 +0100

    First Commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
 // AngularJS App
 
 var myApp = angular.module('myApp', ['ngRoute']);
-
-myApp.config(function ($routeProvider) {
-    $routeProvider
-            .when('/', {templateUrl: 'partials/home.php', controller: 'EtudiantsCtrl'})
-            .when('/etudiant/:idetudiant', {templateUrl: 'partials/etudiant.php', controller: 'EtudiantCtrl'})
-            .otherwise({redirectTo: '/'});
-});
